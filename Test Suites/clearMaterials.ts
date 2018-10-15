7781f37c293cc9dbcb7cd28dfce379056e0fd217
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2018-10-15 15:48:01 +0900

    beautified Test Cases/ImageDiff

--- Test Suites/clearMaterials.ts
+++ Test Suites/clearMaterials.ts
@@ -4,7 +4,7 @@
    <name>clearMaterials</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-10-15T14:59:45</lastRun>
+   <lastRun>2018-10-15T15:46:19</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
