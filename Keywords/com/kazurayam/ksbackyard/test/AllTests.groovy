0fef97e2eaf63184f50128fbbc193c8845076148
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2018-10-17 09:36:02 +0900

    add com.kazurayam.ksbackyard.Version class

--- Keywords/com/kazurayam/ksbackyard/test/AllTests.groovy
+++ Keywords/com/kazurayam/ksbackyard/test/AllTests.groovy
@@ -6,7 +6,13 @@ import org.junit.runners.Suite.SuiteClasses
 
 @RunWith(Suite.class)
 @SuiteClasses([
-	AssertTest.class, ImageCollectionDifferTest.class, QuietBrowserTest.class, RunConfigurationWrapperTest.class, ScreenshotDriverTest.class, URLParserTest.class
+	AssertTest.class,
+	ImageCollectionDifferTest.class,
+	QuietBrowserTest.class,
+	RunConfigurationWrapperTest.class,
+	ScreenshotDriverTest.class, 
+	URLParserTest.class,
+	VersionTest.class
 ])
 // please note. Difference of Groovy and Java here.
 // In Java, this will be written as @SuiteClasses({...}). Difference of Java and Groovy here.
