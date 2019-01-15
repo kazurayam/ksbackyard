f24e6b0db9fc76d3c21bd64fd683ac499d6235ec
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2019-01-15 16:04:40 +0900

    improved AllTestsRunner

--- Scripts/test/AllTestsRunner/Script1547534792211.groovy
+++ Scripts/test/AllTestsRunner/Script1547534792211.groovy
@@ -1,3 +1,17 @@
-import com.kazurayam.ksbackyard.AllTests
+//import com.kazurayam.ksbackyard.AllTests
+//CustomKeywords.'com.kazurayam.ksbackyard.junit.JUnitCustomKeywords.runWithJUnitRunner'(AllTests.class)
 
-CustomKeywords.'com.kazurayam.ksbackyard.junit.JUnitCustomKeywords.runWithJUnitRunner'(AllTests.class)
+
+import com.kazurayam.ksbackyard.AssertTest
+import com.kazurayam.ksbackyard.RunConfigurationWrapperTest
+import com.kazurayam.ksbackyard.TestObjectSupportTest
+import com.kazurayam.ksbackyard.URLParserTest
+import com.kazurayam.ksbackyard.VersionTest
+
+CustomKeywords.'com.kazurayam.ksbackyard.junit.JUnitCustomKeywords.runWithJUnitRunner'(AssertTest.class)
+CustomKeywords.'com.kazurayam.ksbackyard.junit.JUnitCustomKeywords.runWithJUnitRunner'(RunConfigurationWrapperTest.class)
+CustomKeywords.'com.kazurayam.ksbackyard.junit.JUnitCustomKeywords.runWithJUnitRunner'(TestObjectSupportTest.class)
+CustomKeywords.'com.kazurayam.ksbackyard.junit.JUnitCustomKeywords.runWithJUnitRunner'(URLParserTest.class)
+CustomKeywords.'com.kazurayam.ksbackyard.junit.JUnitCustomKeywords.runWithJUnitRunner'(VersionTest.class)
+
+
