41ef82b8b2ca75cb7bdc06a1bb4fd7ad698a4d8a
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2019-01-21 16:14:43 +0900

    corrected version

--- Include/scripts/groovy/com/kazurayam/ksbackyard/VersionTest.groovy
+++ Include/scripts/groovy/com/kazurayam/ksbackyard/VersionTest.groovy
@@ -12,7 +12,7 @@ public class VersionTest {
 
 	@Test
 	void testGetVersion() {
-		def expected = 'ksbackyard 0.17.0'
+		def expected = 'ksbackyard 0.17.2'
 		def actual = Version.getVersion()
 		assertThat(actual, is(expected))
 	}
