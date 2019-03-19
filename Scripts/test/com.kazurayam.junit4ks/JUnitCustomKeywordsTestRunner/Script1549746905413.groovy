aaf4c7652b2a8b4fbd5ce9130a8289175b7baf4e
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2019-03-19 10:11:31 +0900

    impoved build.gradle

--- Scripts/test/com.kazurayam.junit4ks/JUnitCustomKeywordsTestRunner/Script1549746905413.groovy
+++
@@ -1,4 +0,0 @@
-import com.kazurayam.junit4ks.JUnitCustomKeywordsTest
-
-// intentionally failing test
-CustomKeywords.'com.kazurayam.junit4ks.JUnitCustomKeywords.runWithJUnitRunner'(JUnitCustomKeywordsTest.class)
