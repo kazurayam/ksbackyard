edd0d3afcf1b8809449b85127eb2e7c116fdb665
kazurayam <kazuaki.urayama@gmail.com>
2020-03-11 20:24:20 +0900

    #6 RunConfigurationWrapper is deleted

--- Scripts/test/com.kazurayam.ksbackyard/AllTestsRunner/Script1547534792211.groovy
+++ Scripts/test/com.kazurayam.ksbackyard/AllTestsRunner/Script1547534792211.groovy
@@ -5,29 +5,26 @@ import com.kazurayam.ksbackyard.AssertTest
 import com.kazurayam.ksbackyard.ImageCollectionDifferTest
 import com.kazurayam.ksbackyard.OSIdentifierTest
 import com.kazurayam.ksbackyard.QuietBrowserTest
-import com.kazurayam.ksbackyard.RunConfigurationWrapperTest
 import com.kazurayam.ksbackyard.ScreenshotDriverCensoringTest
 import com.kazurayam.ksbackyard.ScreenshotDriverCompareImagesTest
 import com.kazurayam.ksbackyard.ScreenshotDriverOptionsTest
-import com.kazurayam.ksbackyard.ScreenshotDriverResisingTest
+import com.kazurayam.ksbackyard.ScreenshotDriverResizingTest
 import com.kazurayam.ksbackyard.ScreenshotDriverTest
 import com.kazurayam.ksbackyard.TestObjectSupportTest
 import com.kazurayam.ksbackyard.URLParserTest
-import com.kazurayam.ksbackyard.VersionTest
 
 
 runWithJUnitRunner(AssertTest.class)
 runWithJUnitRunner(ImageCollectionDifferTest.class)
 runWithJUnitRunner(OSIdentifierTest.class)
 runWithJUnitRunner(QuietBrowserTest.class)
-runWithJUnitRunner(RunConfigurationWrapperTest.class)
 runWithJUnitRunner(ScreenshotDriverCensoringTest.class)
 runWithJUnitRunner(ScreenshotDriverCompareImagesTest.class)
 runWithJUnitRunner(ScreenshotDriverOptionsTest.class)
-runWithJUnitRunner(ScreenshotDriverResisingTest.class)
+runWithJUnitRunner(ScreenshotDriverResizingTest.class)
 runWithJUnitRunner(ScreenshotDriverTest.class)
 runWithJUnitRunner(TestObjectSupportTest.class)
 runWithJUnitRunner(URLParserTest.class)
-runWithJUnitRunner(VersionTest.class)
+
 
 
--- Scripts/test/com.kazurayam.ksbackyard/RunConfigurationWrapperTestRunner/Script1547534267081.groovy
+++
@@ -1,4 +0,0 @@
-import static com.kazurayam.junit4ks.JUnitCustomKeywords.runWithJUnitRunner
-import com.kazurayam.ksbackyard.RunConfigurationWrapperTest
-
-runWithJUnitRunner(RunConfigurationWrapperTest.class)
